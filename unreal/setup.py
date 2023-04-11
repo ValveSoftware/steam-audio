4bcaafee68613eb3092995fceb43319f864d7544
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2023-04-11 15:21:04 +0200

    [utils/bugfixes] Added lines for copying missing files and Avoiding downloading the zip file If it's already downloaded (otherwise, It throws an exception.)

--- unreal/setup.py
+++ unreal/setup.py
@@ -20,7 +20,8 @@ def download_file(url):
 
 print("Downloading steamaudio_" + version + ".zip...")
 url = "https://github.com/ValveSoftware/steam-audio/releases/download/v" + version + "/steamaudio_" + version + ".zip"
-download_file(url)
+if not os.path.exists("./steamaudio_" + version + ".zip"):
+    download_file(url)
 
 print("Extracting steamaudio_" + version + ".zip...")
 with zipfile.ZipFile(os.path.basename(url), "r") as zip:
@@ -46,7 +47,9 @@ if not os.path.exists("src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudio
 
 print("Copying files...")
 shutil.copy("steamaudio/lib/windows-x86/phonon.dll", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/windows-x86")
+shutil.copy("steamaudio/lib/windows-x86/phonon.lib", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/windows-x86")
 shutil.copy("steamaudio/lib/windows-x64/phonon.dll", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/windows-x64")
+shutil.copy("steamaudio/lib/windows-x64/phonon.lib", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/windows-x64")
 shutil.copy("steamaudio/lib/linux-x86/libphonon.so", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/linux-x86")
 shutil.copy("steamaudio/lib/linux-x64/libphonon.so", "src/SteamAudioUnreal/Plugins/SteamAudio/Source/SteamAudioSDK/lib/linux-x64")
 try:
