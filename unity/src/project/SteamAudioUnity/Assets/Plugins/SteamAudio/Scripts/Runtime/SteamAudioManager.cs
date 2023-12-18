b6c9efe7a29f86cda38f70fddfd0e8431107f3a4
Victor Möller <victor@victormoller.com>
2023-12-18 16:46:09 +0100

    [Unity] Fix for when having SteamAudioGeometry on an object that had a mesh but no sharedmesh
    Common for ProBuilder MeshFilter prefabs

--- unity/src/project/SteamAudioUnity/Assets/Plugins/SteamAudio/Scripts/Runtime/SteamAudioManager.cs
+++ unity/src/project/SteamAudioUnity/Assets/Plugins/SteamAudio/Scripts/Runtime/SteamAudioManager.cs
@@ -1383,7 +1383,7 @@ namespace SteamAudio
             var mesh = gameObject.GetComponent<MeshFilter>();
             var terrain = gameObject.GetComponent<Terrain>();
 
-            if (mesh != null)
+            if (mesh != null && mesh.sharedMesh != null)
             {
                 return mesh.sharedMesh.vertexCount;
             }
@@ -1417,7 +1417,7 @@ namespace SteamAudio
             var mesh = gameObject.GetComponent<MeshFilter>();
             var terrain = gameObject.GetComponent<Terrain>();
 
-            if (mesh != null)
+            if (mesh != null && mesh.sharedMesh != null)
             {
                 return mesh.sharedMesh.triangles.Length / 3;
             }
@@ -1762,7 +1762,7 @@ namespace SteamAudio
             var mesh = gameObject.GetComponent<MeshFilter>();
             var terrain = gameObject.GetComponent<Terrain>();
 
-            if (mesh != null)
+            if (mesh != null && mesh.sharedMesh != null)
             {
                 var vertexArray = mesh.sharedMesh.vertices;
                 for (var i = 0; i < vertexArray.Length; ++i)
@@ -1823,7 +1823,7 @@ namespace SteamAudio
             var mesh = gameObject.GetComponent<MeshFilter>();
             var terrain = gameObject.GetComponent<Terrain>();
 
-            if (mesh != null)
+            if (mesh != null && mesh.sharedMesh != null)
             {
                 var triangleArray = mesh.sharedMesh.triangles;
                 for (var i = 0; i < triangleArray.Length / 3; ++i)
