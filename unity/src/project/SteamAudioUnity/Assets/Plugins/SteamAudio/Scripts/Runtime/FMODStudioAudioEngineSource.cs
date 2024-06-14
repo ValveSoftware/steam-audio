807b894c5c4e39f57d272dc31589ecd06f313d0d
Quentin Leon <qwolf1999@gmail.com>
2024-06-14 12:45:18 -0700

    fixed fmodSource losing DSP every frame (#360)

--- unity/src/project/SteamAudioUnity/Assets/Plugins/SteamAudio/Scripts/Runtime/FMODStudioAudioEngineSource.cs
+++ unity/src/project/SteamAudioUnity/Assets/Plugins/SteamAudio/Scripts/Runtime/FMODStudioAudioEngineSource.cs
@@ -83,7 +83,7 @@ namespace SteamAudio
             if (mEventEmitter != null)
             {
                 var eventInstance = FMODUnity_StudioEventEmitter_EventInstance.GetValue(mEventEmitter, null);
-                if (eventInstance != mEventInstance)
+                if (!eventInstance.Equals(mEventInstance))
                 {
                     // The event instance is different from the one we last used, which most likely means the
                     // event-related objects were destroyed and re-created. Make sure we look for the DSP instance
